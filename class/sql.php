dea30cb7b25333a48cc030c72b2e498486a171a6
lourenco-tx <lourenco.tx@gmail.com>
2017-12-08 23:50:55 -0200

    Criado Class Usuario

--- class/sql.php
+++ class/sql.php
@@ -5,13 +5,13 @@
 		public function __construct(){
 			$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");		
 		}
-		private function setParams($statment, $paramenters = array()){
+		private function setParams($statement, $paramenters = array()){
 			foreach ($paramenters as $key => $value) {
-				$this->setParam($key, $value);
+				$this->setParam($statement, $key, $value);
 			}
 		}
-		private function setParam($statment, $key, $value){
-			$statment->blindParam($key, $value);
+		private function setParam($statement, $key, $value){
+			$statement->bindParam($key, $value);
 		}
 		public function query($rawQuery, $params = array()){
 			$stmt = $this->conn->prepare($rawQuery);
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 	require_once("config.php");
-	$sql = new sql();
-	$usuarios = $sql->select("SELECT * from tb_usuarios");
-	echo json_encode($usuarios);
+	$exibe = new Usuario();
+	$exibe -> loadById(3);
+	echo $exibe;
+
+	//Select simples de todos os dados
+	//$sql = new sql();
+	//$usuarios = $sql->select("SELECT * from tb_usuarios");
+	//echo json_encode($usuarios);
+
 ?>
